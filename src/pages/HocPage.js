7849ad94a53f62decfd0cab6177abf8feb42f0b9
liwen <liwen111111>
2021-04-10 16:30:48 +0800

    高阶组件及react使用装饰器配置

--- src/pages/HocPage.js
+++ src/pages/HocPage.js
@@ -1,16 +1,6 @@
 // ⾼阶组件HOC: 是一个函数，接收一个组件返回一个新组件
 import React, { Component } from 'react';
 
-@foo
-class Child extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <h1>Child Component</h1>
-     );
-  }
-}
-
 const foo = (Cmp) => (props) => {
   console.log(props)
   return (
@@ -28,6 +18,15 @@ const foo2 = (Cmp) => (props) => {
     </div>
   )
 }
+@foo2
+@foo
+class Child extends Component {
+  render() { 
+    return ( 
+      <h1>Child Component</h1>
+     );
+  }
+}
 
 // const Foo = foo2(foo(child))
 class HocPage extends Component {
@@ -39,7 +38,7 @@ class HocPage extends Component {
     return (
       <div>
         <p>HocPage</p>
-        <Child />
+        <Child name={'444'}/>
       </div>
     );
   }
